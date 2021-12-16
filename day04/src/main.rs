28872f0c50cbf200b48edd504dac6ff941ccf95e
Dale Lim <dalelim.94+github@gmail.com>
2021-12-17 02:38:31 +1100

    wip day 4: first answer

--- day04/src/main.rs
+++ day04/src/main.rs
@@ -13,11 +13,64 @@ struct Board {
 impl Board {
     fn push_row(&mut self, row: Vec<i32>) {
         self.numbers.push(row);
+        self.marks.push(vec![false; BOARD_SIZE]);
+    }
+
+    fn mark_number(&mut self, number: i32) {
+        for i in 0..self.numbers.len() {
+            for j in 0..self.numbers[i].len() {
+                if self.numbers[i][j] == number {
+                    self.marks[i][j] = true;
+                    return;
+                }
+            }
+        }
+    }
+
+    fn is_bingo(&mut self) -> bool {
+        // check each row
+        'outer: for i in 0..self.marks.len() {
+            let row = &self.marks[i];
+            for j in 0..row.len() {
+                if j == row.len() - 1 && row[j] {
+                    return true;
+                } else if row[j] == false {
+                    continue 'outer;
+                }
+            }
+        }
+
+        // check each column
+        'outer: for i in 0..self.marks.len() {
+            for j in 0..BOARD_SIZE {
+                if j == BOARD_SIZE - 1 && self.marks[j][i] {
+                    return true;
+                } else if self.marks[j][i] == false {
+                    continue 'outer;    
+                }
+            }
+        }
+
+        return false;
+    }
+
+    fn compute_score(&mut self, final_number: i32) -> i32 {
+        let mut sum = 0;
+        for i in 0..self.marks.len() {
+            let row = &self.marks[i];
+            for j in 0..row.len() {
+                if self.marks[i][j] == false {
+                    sum += self.numbers[i][j];
+                }
+            }
+        }
+
+        return sum * final_number;
     }
 }
 
 fn main() -> std::io::Result<()> {
-    let file = File::open("test-input")?;
+    let file = File::open("input")?;
     let lines = BufReader::new(file).lines();
 
     let lines_vec: Vec<String> = lines.map(|s| s.unwrap()).collect();
@@ -60,8 +113,20 @@ fn main() -> std::io::Result<()> {
 
     println!("draws: {:?}", draws);
 
-    for board in boards.iter() {
-        dbg!(&board);
+//    for board in boards.iter() {
+//        dbg!(&board);
+//    }
+
+    // first answer
+    'outer: for draw in draws {
+        for board in boards.iter_mut() {
+            board.mark_number(draw);
+            if board.is_bingo() {
+                println!("answer is: {}", board.compute_score(draw));
+                break 'outer;
+            }
+        }
+        println!("draw {}:  board: {:?}", draw, boards[0]);
     }
 
     Ok(())
