fb412973326bb36305dc2659fb27a585ba3f4ed9
Dale Lim <dalelim.94+github@gmail.com>
2021-12-19 14:10:33 +1100

    Day 6 solution

--- day06/src/main.rs
+++ day06/src/main.rs
@@ -1,38 +1,53 @@
+use std::collections::HashMap;
 use std::fs::File;
 use std::io::prelude::*;
 use std::io::BufReader;
 
+fn generated_fishes(
+    starting_fish: i32,
+    days: i32,
+    cache: &mut HashMap<(i32, i32), usize>,
+) -> usize {
+    if starting_fish >= days {
+        return 0;
+    }
+    if cache.contains_key(&(starting_fish, days)) {
+        return cache[&(starting_fish, days)];
+    }
+
+    let first = generated_fishes(6, days - starting_fish - 1, cache);
+    let second = generated_fishes(8, days - starting_fish - 1, cache);
+
+    let fishes = 1 + first + second;
+    cache.insert((starting_fish, days), fishes);
+    return fishes;
+}
+
 fn main() -> std::io::Result<()> {
-    let file = File::open("test-input")?;
+    let file = File::open("input")?;
     let lines = BufReader::new(file).lines();
 
     let lines_vec: Vec<String> = lines.map(|s| s.unwrap()).collect();
 
-    let mut fishes = lines_vec.get(0).unwrap().split(",").map(|num| num.parse::<i32>().unwrap()).collect::<Vec<i32>>();
+    let fishes = lines_vec
+        .get(0)
+        .unwrap()
+        .split(",")
+        .map(|num| num.parse::<i32>().unwrap())
+        .collect::<Vec<i32>>();
 
     println!("Initial state: {:?}", fishes);
 
-    let days = 80;
+    let days = 256;
+    let mut fish_count = 0;
 
-    for day in 0..days {
-        for i in 0..fishes.len() {
-            let mut to_add = 0;
-            fishes[i] = fishes[i] - 1;
-            if fishes[i] < 0 {
-                fishes[i] = 6;
-                to_add += 1;
-            }
+    let mut count_map: HashMap<(i32, i32), usize> = HashMap::new();
 
-            for _ in 0..to_add {
-                fishes.push(8)
-            }
-        }
-
-        println!("After {} day(s): {:?}", day+1, fishes);
-        println!("Day {} - Fish count: {}", day+1, fishes.len());
+    for fish in fishes {
+        fish_count += generated_fishes(fish, days, &mut count_map) + 1;
     }
-    
-    println!("Day {} - Fish count: {}", days, fishes.len());
+
+    println!("Day {} - Fish count: {}", days, fish_count);
 
     Ok(())
 }
