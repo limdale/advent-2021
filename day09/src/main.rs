f6832fe676b05043728a455c52061743cf8df74e
Dale Lim <dalelim.94+github@gmail.com>
2021-12-25 18:08:01 +1100

    Day 9 solution

--- day09/src/main.rs
+++ day09/src/main.rs
@@ -1,6 +1,41 @@
+use std::collections::HashSet;
 use std::fs::File;
 use std::io::prelude::*;
 use std::io::BufReader;
+
+fn get_basin_size(heightmap: &Vec<Vec<usize>>, low_point: (usize, usize)) -> usize {
+    let mut visited: HashSet<(usize, usize)> = HashSet::new();
+    let mut to_visit: Vec<(usize, usize)> = Vec::new();
+    to_visit.push(low_point);
+
+    while !to_visit.is_empty() {
+        let (x, y) = to_visit.pop().unwrap();
+
+        if !visited.contains(&(x, y)) {
+            visited.insert((x, y));
+        } else {
+            continue;
+        }
+
+        if y > 0 && heightmap[(y - 1)][x] < 9 {
+            to_visit.push((x, y - 1));
+        }
+
+        if y < heightmap.len() - 1 && heightmap[y + 1][x] < 9 {
+            to_visit.push((x, y + 1));
+        }
+
+        if x > 0 && heightmap[y][x - 1] < 9 {
+            to_visit.push((x - 1, y));
+        }
+
+        if x < heightmap[y].len() - 1 && heightmap[y][x + 1] < 9 {
+            to_visit.push((x + 1, y));
+        }
+    }
+
+    return visited.len();
+}
 
 fn main() -> std::io::Result<()> {
     let file = File::open("input")?;
@@ -8,18 +43,19 @@ fn main() -> std::io::Result<()> {
 
     let lines_vec: Vec<String> = lines.map(|s| s.unwrap()).collect();
 
-    let mut heightmap: Vec<Vec<i32>> = Vec::new();
+    let mut heightmap: Vec<Vec<usize>> = Vec::new();
 
     for line in lines_vec {
-        let mut row: Vec<i32> = Vec::new();
+        let mut row: Vec<usize> = Vec::new();
         for c in line.chars() {
-            let num = c.to_string().parse::<i32>().unwrap();
+            let num = c.to_string().parse::<usize>().unwrap();
             row.push(num);
         }
         heightmap.push(row);
     }
 
     let mut sum = 0;
+    let mut low_points: Vec<(usize, usize)> = Vec::new();
 
     for i in 0..heightmap.len() {
         for j in 0..heightmap[i].len() {
@@ -49,12 +85,29 @@ fn main() -> std::io::Result<()> {
                 //    heightmap[i][j], i, j, left, right, top, bottom
                 //);
                 sum += heightmap[i][j] + 1;
+                low_points.push((j as usize, i as usize));
             }
         }
     }
 
-    //println!("{:?}", heightmap);
     println!("Sum: {}", sum);
+    //println!("Low points: {:?}", low_points);
+
+    let mut basin_sizes: Vec<usize> = Vec::new();
+
+    for low_point in low_points {
+        let basin_size = get_basin_size(&heightmap, low_point);
+        //println!(
+        //    "Basin size for low point ({}, {}): {}",
+        //    low_point.0, low_point.1, basin_size
+        //);
+        basin_sizes.push(basin_size);
+    }
+
+    basin_sizes.sort();
+
+    let length = basin_sizes.len();
+    println!("Answer: {}", basin_sizes[length - 1] * basin_sizes[length - 2] * basin_sizes[length - 3]);
 
     Ok(())
 }
